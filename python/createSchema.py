ad5dee24bc30f268013de44ecb342cd76fa9b264
zack <you@example.com>
2019-09-09 14:02:56 +0000

    switched to connections.config construction of connection

--- python/createSchema.py
+++ python/createSchema.py
@@ -1,24 +1,30 @@
 import pyodbc
+import os
 
 
 
 def connect_to_iris():
     # Login credentials
-    driver = "{InterSystems ODBC}"
-    ip = "172.24.2.254"
-    port = 51773
-    namespace = "USER"
-    username = "SuperUser"
-    password = "SYS"
 
+    connection = get_connection_config()
     # Connect to InterSystems IRIS using PyODBC
     connection_string = 'DRIVER={};SERVER={};PORT={};DATABASE={};UID={};PWD={}'\
-        .format(driver, ip, port, namespace, username, password)
+        .format(connection['driver'], connection['ip'], int(connection['port']),\
+         connection['namespace'], connection['username'], connection['password'])
     pyodbc_connection = pyodbc.connect(connection_string)
 
     print("Connected to InterSystem IRIS")
     return pyodbc_connection
 
+def get_connection_config():
+    with open("../connections.config") as f:
+        f = f.read()
+        f = f.split("\n")
+        dict = {}
+        for line in f:
+            line = line.split(":")
+            dict[line[0].strip()] = line[1].strip()
+        return dict
 
 # Remove old table if exist
 def delete_old_table(cursor, table_name):
@@ -41,11 +47,13 @@ def create_employee(connection):
     """
     try:
         cursor.execute(create_employee)
-    except:
+    except Exception as e:
+        print("need to delete old table",e)
         delete_old_table(cursor, "Demo.Employee")
         cursor.execute(create_employee)
 
     connection.commit()
+    print("created table successfully")
 
 def run():
     # Get connections for PyODBC
